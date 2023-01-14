615b6f2ae0d8ccc9a8141d78a7b2efa4082e76e6
Lake <59345467+Lakelimbo@users.noreply.github.com>
2023-01-14 19:56:24 -0300

    didn't need a base actually

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lakelimbo.github.io',
-    base: '/lakelimbo.github.io'
 });
