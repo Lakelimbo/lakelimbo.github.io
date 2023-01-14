09ccaeaade62917cff3a67ddb9a1975ad8491e2c
Lakelimbo <gabrielmarcopolo@gmail.com>
2023-01-14 18:38:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lake's website
+This is my personal website, mostly used as a mini portfolio.
+
+* It's built on top of [Astro](https://astro.build/)
+* Contains a very barebones i18 for my native language (Portuguese)
+* Some icons come from [Devicon](https://devicon.dev/)
---
+++ src/scripts/experience.ts
@@ -0,0 +1,130 @@
+export const programming = {
+   section: {
+      en: "Programming",
+      pt: "Programação",
+   },
+   blocks: [
+      {
+         name: "HTML",
+         icon: "devicon-html5-plain",
+         color: "#e44d26",
+      },
+      {
+         name: "CSS",
+         icon: "devicon-css3-plain",
+         color: "#1572b6",
+      },
+      {
+         name: "JavaScript",
+         icon: "devicon-javascript-plain",
+         color: "#f0db4f",
+      },
+      {
+         name: "TypeScript",
+         icon: "devicon-typescript-plain",
+         color: "#007acc",
+      },
+      {
+         name: "Node.js",
+         icon: "devicon-nodejs-plain",
+         color: "#3ed30b",
+      },
+      {
+         name: "React",
+         icon: "devicon-react-plain",
+         color: "#007acc",
+      },
+      {
+         name: "Next.js",
+         icon: "devicon-nextjs-plain",
+         color: "#fff",
+      },
+      {
+         name: "Astro",
+         icon: "astro-logo",
+         color: "#b82c1f",
+      },
+      {
+         name: "PostgreSQL",
+         icon: "devicon-postgresql-plain",
+         color: "#1161d6",
+      },
+      {
+         name: "SQLite",
+         icon: "devicon-sqlite-plain",
+         color: "#11bdd6",
+      },
+      {
+         name: "Lua",
+         icon: "devicon-lua-plain",
+         color: "#2195e0",
+      },
+      {
+         name: "VS Code",
+         icon: "devicon-vscode-plain",
+         color: "#3c99d4",
+      },
+      {
+         name: "Wordpress",
+         icon: "devicon-wordpress-plain",
+         color: "#fff",
+      },
+      {
+         name: "MediaWiki",
+         icon: "mediawiki-logo",
+         color: "#f27656",
+      },
+      {
+         name: "Bootstrap",
+         icon: "devicon-bootstrap-plain",
+         color: "#a338f7",
+      },
+      {
+         name: "TailwindCSS",
+         icon: "devicon-tailwindcss-plain",
+         color: "#0ff7c7",
+      },
+      {
+         name: "Sass/SCSS",
+         icon: "devicon-sass-original",
+         color: "#e426c9",
+      },
+   ],
+   content: {
+      en: "Minor experience includes: Python, Ruby, GDScript, VB.NET, Vue.js, Deno, etc...",
+      pt: "Experiência menores incluem: Python, Ruby, GDSCript, VB.Net, Vue.js, Deno, etc...",
+   },
+};
+
+export const design = {
+   section: {
+      en: "Design",
+      pt: "Design",
+   },
+   blocks: [
+      {
+         name: "Photoshop",
+         icon: "devicon-photoshop-plain",
+         color: "#0c63cd",
+      },
+      {
+         name: "Illustrator",
+         icon: "devicon-illustrator-plain",
+         color: "#dc940b",
+      },
+      {
+        name: "Adobe XD",
+        icon: "devicon-xd-plain",
+        color: "#e865ff",
+     },
+      {
+        name: "Figma",
+        icon: "devicon-figma-plain",
+        color: "#ff3383",
+     },
+   ],
+   content: {
+      en: "Other experiences includes: ClipStudio Paint, Paint Tool SAI, Lunacy, InDesign...",
+      pt: "Outras experiências incluem: ClipStudio Paint, Paint Tool SAI, Lunacy, InDesign...",
+   },
+};
