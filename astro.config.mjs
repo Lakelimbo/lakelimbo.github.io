0fbc14efa9bb148cf15e63592b5c0b9309d9ad0e
Lakelimbo <gabrielmarcopolo@gmail.com>
2023-01-14 19:50:00 -0300

    updating astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lakelimbo.github.io',
-    base: '/lakelimbo'
+    base: '/lakelimbo.github.io'
 });
